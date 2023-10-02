76002f37436605bb2df2eae01d5c2a60071f65ea
Squabe517 <gaigeawycuff@gmail.com>
2023-10-02 12:37:01 -0400

    Initial

---
+++ readme.md
@@ -0,0 +1,46 @@
+# Text Summarizer using GPT-3.5 Turbo
+
+This script reads a long text from a file, splits it into manageable chunks, and uses the OpenAI GPT-3.5 Turbo model to generate a summary. The summarized text is then written to an output file.
+
+## Prerequisites
+
+- Python 3.x
+- OpenAI Python package
+- Tiktoken Python package
+- python-dotenv package
+
+You can install the necessary Python packages using pip:
+
+```bash
+pip install openai tiktoken python-dotenv
+```
+
+## Environment Variables
+
+Create a `.env` file in your project directory and add your OpenAI API key:
+
+```env
+API_KEY=your_openai_api_key_here
+```
+
+## Usage
+
+1. Place the text you want to summarize in a file called `text.txt` in the project directory.
+2. Run the script:
+
+```bash
+python reducer.py
+```
+
+3. The summarized text will be written to `output.txt`.
+
+## How It Works
+
+1. **Read Text**: The `readFile` function reads the text from `text.txt`.
+2. **Tokenize and Split**: Tiktoken is used to tokenize the text and split it into smaller chunks.
+3. **Generate Summary**: Each chunk is sent to GPT-3.5 Turbo to generate a summary.
+4. **Write Summary**: The `write_out_response` function writes the summary to `output.txt`.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ reducer.py
@@ -0,0 +1,69 @@
+import openai
+import tiktoken as tk
+from dotenv import load_dotenv
+import os
+
+# GET API KEY FROM .ENV FILE
+load_dotenv()
+api_key = os.environ.get("API_KEY")
+
+# SET OPENAI API KEY
+openai.api_key = api_key
+
+
+# Reads a text file and returns the contents as a string
+def readFile(textFile):
+    print("reading data from file...\n")
+    # Read the file in binary mode
+    with open(textFile, 'rb') as file:
+        byte_data = file.read()
+    # Decode the bytes to string, preserving escape sequences
+    text_data = byte_data.decode('unicode_escape')
+    return text_data
+
+# Splits a list into smaller lists of a given size then returns the list of lists
+def split_list(long_list):
+    print("splitting list...\n")
+    max_len = 1000
+    sublists = [long_list[i:i + max_len] for i in range(0, len(long_list), max_len)]
+    return sublists
+
+# Completely overwrites output file then writes the summary to it
+def write_out_response(response):
+    print("writing response to file...\n")
+    with open('output.txt', 'w') as file:
+        pass  # File is emptied by opening it in write mode
+
+    with open('output.txt', 'w') as file:
+        for string in summary:
+            file.write(string + '\n')  # Add a newline after each string
+
+# Read the file in binary mode
+text_data = readFile('text.txt')
+
+# Encode the text and see how many tokens it contains
+encoding = tk.encoding_for_model('gpt-3.5-turbo')
+tokenizedText = encoding.encode(text_data)
+
+# Split the text into smaller chunks, then decode it
+splitText = split_list(tokenizedText)
+for i in range(len(splitText)):
+  splitText[i] = encoding.decode(splitText[i])
+
+
+# Send messages to chatbot, then append responses to list
+summary = []
+for message in splitText:
+    print("sending message to chatbot...(" + str(splitText.index(message)) + "/" + str(len(splitText)) + ")\n")
+    myMessage = [
+            {"role": "system", "content": "You are a college student who has been asked to summarize the following text:"},
+            {'role': 'user', 'content': message},
+    ]
+    print("receiving response from chatbot...\n")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages= myMessage 
+    )
+    summary.append(response['choices'][0]['message']['content'])
+    
+write_out_response(summary)
